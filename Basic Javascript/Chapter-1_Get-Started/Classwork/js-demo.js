54e4b41c59ca0a8013a004e62afe6180ae9d6622
tramnnb-qc <tramnnbqc@gmail.com>
2026-04-12 15:37:50 +0700

    Initial commit: add automation exercises

---
+++ Basic Javascript/Chapter-1_Get-Started/Classwork/js-demo.js
@@ -0,0 +1,2 @@
+console.log("Chào mừng bạn đến với thế giới Automation!");
+console.log("Hôm nay là ngày: " + new Date().toLocaleDateString());
---
+++ Basic Javascript/Chapter-1_Get-Started/Homework/calculator.js
@@ -0,0 +1,4 @@
+const a = 2;
+const b = 5;
+console.log("Tổng của " + a + " và " + b + " là: " + (a + b));
+console.log("Tổng của ", a, " và ", b, " là: ", a + b);
